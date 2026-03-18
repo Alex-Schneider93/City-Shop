59f8c4d0dad40fbacabd486db5290fcaa7b58dd8
Alex-Schneider93 <schneideralexandru5@gmail.com>
2026-03-18 15:58:14 +0100

    Change database connection details in config.php
    Updated database configuration with new credentials.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    "host" => "database-5020023421.webspace-host.com",
-    "dbname" => "dbs15441995",
-    "user" => "dbu844986",
+    "host" => "*****************",
+    "dbname" => "*************",
+    "user" => "**********",
     "password" => "***********"
 ];
