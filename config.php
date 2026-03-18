d22de993eafc718a4a48b60d377a6b1dbbc455c4
Alex-Schneider93 <schneideralexandru5@gmail.com>
2026-03-18 15:55:16 +0100

    Hide password in config.php
    Obscured the password in the configuration file for security.

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ return [
     "host" => "database-5020023421.webspace-host.com",
     "dbname" => "dbs15441995",
     "user" => "dbu844986",
-    "password" => "MeinpassworT$1"
+    "password" => "***********"
 ];
